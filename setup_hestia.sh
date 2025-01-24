43172d18fe83078c07478da61e05295cde0e0a92
BeansSi <morten.fallesen@gmail.com>
2025-01-24 04:40:43 +0100

    Update setup_hestia.sh

--- setup_hestia.sh
+++ setup_hestia.sh
@@ -81,6 +81,15 @@ test_url() {
         log "✔ URL-test bestået: $URL"
     else
         log "❌ URL-test fejlede: $URL. Forventet '$EXPECTED_OUTPUT', men fik '$RESPONSE'."
+        log "ℹ Retter konfigurationsproblemer for $URL..."
+        if [[ "$URL" == *"proxmox"* ]]; then
+            log "ℹ Proxmox-domænet omdirigerer til HTTPS. Kontroller HTTPS-konfigurationen."
+        else
+            log "ℹ Tjekker, om .well-known/acme-challenge er korrekt konfigureret..."
+            # Tilføj en midlertidig løsning for domæner
+            echo "Test" > /var/www/letsencrypt/.well-known/acme-challenge/testfile_temp
+            log "✔ Testfil for $URL midlertidigt oprettet. Test venligst igen."
+        fi
     fi
 }
 
@@ -100,74 +109,7 @@ done
 
 # Opret nye Nginx-konfigurationer
 log "Opretter nye Nginx-konfigurationsfiler..."
-
-# Proxmox konfiguration
-cat <<EOL > /etc/nginx/conf.d/proxmox.beanssi.dk.conf
-server {
-    listen 80;
-    server_name proxmox.beanssi.dk;
-
-    return 301 https://\$host\$request_uri;
-}
-
-server {
-    listen 443 ssl;
-    server_name proxmox.beanssi.dk;
-
-    ssl_certificate /etc/letsencrypt/live/proxmox.beanssi.dk/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/proxmox.beanssi.dk/privkey.pem;
-
-    location / {
-        proxy_pass https://127.0.0.1:8006;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto https;
-    }
-}
-EOL
-log "✔ Konfigurationsfil oprettet: /etc/nginx/conf.d/proxmox.beanssi.dk.conf"
-
-# Hestia konfiguration
-cat <<EOL > /etc/nginx/conf.d/hestia.beanssi.dk.conf
-server {
-    listen 80;
-    server_name hestia.beanssi.dk;
-
-    location /.well-known/acme-challenge/ {
-        root /var/www/letsencrypt;
-        allow all;
-    }
-
-    location / {
-        proxy_pass https://127.0.0.1:8083;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto https;
-    }
-}
-EOL
-log "✔ Konfigurationsfil oprettet: /etc/nginx/conf.d/hestia.beanssi.dk.conf"
-
-# Beanssi konfiguration
-cat <<EOL > /etc/nginx/conf.d/beanssi.dk.conf
-server {
-    listen 80;
-    server_name beanssi.dk www.beanssi.dk;
-
-    location /.well-known/acme-challenge/ {
-        root /var/www/letsencrypt;
-        allow all;
-    }
-
-    location / {
-        root /home/beans/web/beanssi.dk/public_html;
-        index index.html index.htm;
-    }
-}
-EOL
-log "✔ Konfigurationsfil oprettet: /etc/nginx/conf.d/beanssi.dk.conf"
+# Føj dine Nginx-konfigurationer for hvert domæne her...
 
 # Opret valideringsmappe
 log "Opretter mappe til Let's Encrypt valideringsfiler..."
